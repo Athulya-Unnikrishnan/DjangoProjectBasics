6afcece90fbe33b0a840a62b98e6b4feb66a9aed
Athulya-Unnikrishnan <athulyau555@gmail.com>
2021-01-28 11:13:24 +0530

    second commit

---
+++ LaanguageFundamentals/flowcontrols/decisionmaking/ifelse.py
@@ -0,0 +1,5 @@
+Age=int(input("Enter the age"))
+if(Age>=18):
+    print("U are eligible for voting")
+else:
+    print("U are not eligible for voting")
---
+++ LaanguageFundamentals/flowcontrols/decisionmaking/trueorfalse.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter a number"))
+num2=int(input("Enter a number"))
+sum=num1+num2
+if(sum==100)|(num1==100)|(num2==100):
+    print("True")
+else:
+    print("False")
